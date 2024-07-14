be46030efde3ab8d1c53d32cc1c605d07ad0eaca
Alex <alexmpa@yandex.ru>
2024-07-14 17:54:14 -0300

    Example: Tweak gas limit

--- examples/main.go
+++ examples/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	// build script
 	sb := scriptbuilder.BeginScript()
-	script := sb.AllowGas(kp.Address().String(), "", 100000, 1200).
+	script := sb.AllowGas(kp.Address().String(), "", 100000, 21000).
 		TransferTokens("SOUL", kp.Address().String(), "ADD_RECEIVER_HERE", 100000000).
 		SpendGas(kp.Address().String()).
 		EndScript()
