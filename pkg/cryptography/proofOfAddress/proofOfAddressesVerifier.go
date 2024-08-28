67cff4ae0b634826405be42d2b3f709f4006b8b7
Alex <alexmpa@yandex.ru>
2024-08-28 19:06:46 -0300

    Add ProofOfAddressesVerifier for Phantasma and Ethereum (WIP)

---
+++ pkg/cryptography/proofOfAddress/proofOfAddressesVerifier.go
@@ -0,0 +1,134 @@
+package proofOfAddress
+
+import (
+	"crypto/ed25519"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/phantasma-io/phantasma-go/pkg/cryptography"
+	"github.com/phantasma-io/phantasma-go/pkg/cryptography/ecdsa"
+)
+
+type ProofOfAddressesVerifier struct {
+	Message            string
+	SignedMessage      string
+	SignedMessageBytes []byte
+
+	PhaAddress        string
+	PhaPublicKeyBytes []byte
+
+	EthAddress        string
+	EthPublicKey      string
+	EthPublicKeyBytes []byte
+
+	Neo2Address        string
+	Neo2PublicKey      string
+	Neo2PublicKeyBytes []byte
+
+	PhaSignature       string
+	PhaSignatureBytes  []byte
+	EthSignature       string
+	EthSignatureBytes  []byte
+	Neo2Signature      string
+	Neo2SignatureBytes []byte
+}
+
+func NewProofOfAddressesVerifier(message string) *ProofOfAddressesVerifier {
+	v := &ProofOfAddressesVerifier{}
+	v.Message = message
+
+	split := strings.Split(strings.Replace(v.Message, "\r", "", -1), "\n")
+
+	v.SignedMessage = strings.Join(split[:6], "\n")
+	fmt.Printf("v.SignedMessage: %s\n", v.SignedMessage)
+	v.SignedMessageBytes = []byte(v.SignedMessage)
+
+	v.PhaAddress = split[1][19:]
+	fmt.Printf("v.PhaAddress: %s\n", v.PhaAddress)
+	phaAddress, err := cryptography.FromString(v.PhaAddress)
+	if err != nil {
+		panic(err)
+	}
+	v.PhaPublicKeyBytes = phaAddress.GetPublicKey()
+
+	v.EthAddress = split[2][18:]
+	fmt.Printf("v.EthAddress: %s\n", v.EthAddress)
+	v.EthPublicKey = split[3][21:]
+	fmt.Printf("v.EthPublicKey: %s\n", v.EthPublicKey)
+	v.EthPublicKeyBytes, err = hex.DecodeString(v.EthPublicKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("v.EthPublicKey (HEX): 0x%s\n", hex.EncodeToString(v.EthPublicKeyBytes))
+
+	v.Neo2Address = split[4][20:]
+	fmt.Printf("v.Neo2Address: %s\n", v.Neo2Address)
+	v.Neo2PublicKey = split[5][23:]
+	fmt.Printf("v.Neo2PublicKey: %s\n", v.Neo2PublicKey)
+	v.Neo2PublicKeyBytes, err = hex.DecodeString(v.Neo2PublicKey)
+	if err != nil {
+		panic(err)
+	}
+	v.PhaSignature = split[7][21:]
+	fmt.Printf("v.PhaSignature: %s\n", v.PhaSignature)
+	v.PhaSignatureBytes, err = hex.DecodeString(v.PhaSignature)
+	if err != nil {
+		panic(err)
+	}
+	v.EthSignature = split[8][20:]
+	fmt.Printf("v.EthSignature: %s\n", v.EthSignature)
+	v.EthSignatureBytes, err = hex.DecodeString(v.EthSignature)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("v.EthSignatureBytes len: %d\n", len(v.EthSignatureBytes))
+	fmt.Printf("v.EthSignature (HEX): 0x%s\n", hex.EncodeToString(v.EthSignatureBytes))
+
+	v.Neo2Signature = split[9][22:]
+	fmt.Printf("v.Neo2Signature: %s\n", v.Neo2Signature)
+	v.Neo2SignatureBytes, err = hex.DecodeString(v.Neo2Signature)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (v *ProofOfAddressesVerifier) VerifyMessage() (bool, string) {
+	success := true
+	errorMessage := ""
+
+	if !ed25519.Verify(v.PhaPublicKeyBytes, v.SignedMessageBytes, v.PhaSignatureBytes) {
+		success = false
+		errorMessage += "Phantasma signature is incorrect!\n"
+	}
+
+	fmt.Println(len(v.EthSignatureBytes))
+	if !ecdsa.Verify(v.SignedMessageBytes, v.EthSignatureBytes, v.EthPublicKeyBytes, ecdsa.Secp256k1) {
+		success = false
+		errorMessage += "Ethereum signature is incorrect!\n"
+	}
+
+	/*	if (!ECDsa.Verify(SignedMessageBytes, Neo2SignatureBytes, Neo2PublicKeyBytes, ECDsaCurve.Secp256r1))
+		{
+		    success = false;
+		    errorMessage += "Neo Legacy signature is incorrect!\n";
+		}
+
+		var ethAddressFromPublicKey = new Poltergeist.PhantasmaLegacy.Ethereum.Util.AddressUtil().ConvertToChecksumAddress(EthereumKey.PublicKeyToAddress(EthPublicKeyBytes));
+		if (EthAddress != ethAddressFromPublicKey)
+		{
+		    success = false;
+		    errorMessage += "Ethereum address is incorrect: " + ethAddressFromPublicKey + "\n";
+		}
+
+		var neo2AddressFromPublicKey = Poltergeist.Neo2.Core.NeoKeys.PublicKeyToN2Address(Neo2PublicKeyBytes);
+		if (Neo2Address != neo2AddressFromPublicKey)
+		{
+		    success = false;
+		    errorMessage += "Neo Legacy address is incorrect: " + neo2AddressFromPublicKey + "\n";
+						}*/
+
+	return success, errorMessage
+}
---
+++ pkg/cryptography/proofOfAddress/proofOfAddressesVerifier_test.go
@@ -0,0 +1,57 @@
+package proofOfAddress
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testMessage string = `I have signed this message with my Phantasma, Ethereum and Neo Legacy signatures to prove that following addresses belong to me and were derived from private key that belongs to me and to confirm my willingness to swap funds across these addresses upon my request. My public addresses are:
+Phantasma address: P2KHhbVZWDv1ZLLoJccN3PUAb9x9BqRnUyH3ZEhu5YwBeJQ
+Ethereum address: 0xDf738B927DA923fe0A5Fd3aD2192990C68913e6a
+Ethereum public key: 5D3F7F469803C68C12B8F731576C74A9B5308484FD3B425D87C35CAED0A2E398C7AC626D916A1D65E23F673A55E6B16FFC1ABD673F3EF6AE8D5E6A0F99784A56
+Neo Legacy address: Ae3aEA6CpvckvypAUShj2CLsy7sfynKUzj
+Neo Legacy public key: 183A301779007BF42DD7B5247587585B0524E13989F964C2A8E289A0CDC91F001765FCC3B4CEE5ED274C4A8B6D80978BDFED678210458CE264D4A4DAB3923EE6
+
+Phantasma signature: EEC4FFE0FE71C522E32A99C3C84F111944275D967BA74BA06749CB58EEC48A2E450B0ABCC59D86880AC01DA2AECFEF1FC5C45BFD840761FC420AF8194ACD5B01
+Ethereum signature: E3E1FCD85385675F9E3508630570C545DECCD1241C7A8FFF523D2AC500D6F68745E43975DBF871C99504100B8DD6715F036FA51EFF9EB8B79D1E31FD555E78FC
+Neo Legacy signature: 8A961AB366DFD9A3EB1ED5FD496E1CE818A67873162A7A21F663BA320324A8B075EAFA432926B7A894FB3DC5138BF0564A10D1AAC1A4B4E8DB8184CD69F829AC`
+
+var testMessagePhaAddressIncorrect string = `I have signed this message with my Phantasma, Ethereum and Neo Legacy signatures to prove that following addresses belong to me and were derived from private key that belongs to me and to confirm my willingness to swap funds across these addresses upon my request. My public addresses are:
+Phantasma address: P2KHhbVZWDv1ZLLoJccN3PUAb9x9BqRnUyH3ZEhu5YwBeJW
+Ethereum address: 0xDf738B927DA923fe0A5Fd3aD2192990C68913e6a
+Ethereum public key: 5D3F7F469803C68C12B8F731576C74A9B5308484FD3B425D87C35CAED0A2E398C7AC626D916A1D65E23F673A55E6B16FFC1ABD673F3EF6AE8D5E6A0F99784A56
+Neo Legacy address: Ae3aEA6CpvckvypAUShj2CLsy7sfynKUzj
+Neo Legacy public key: 183A301779007BF42DD7B5247587585B0524E13989F964C2A8E289A0CDC91F001765FCC3B4CEE5ED274C4A8B6D80978BDFED678210458CE264D4A4DAB3923EE6
+
+Phantasma signature: EEC4FFE0FE71C522E32A99C3C84F111944275D967BA74BA06749CB58EEC48A2E450B0ABCC59D86880AC01DA2AECFEF1FC5C45BFD840761FC420AF8194ACD5B01
+Ethereum signature: E3E1FCD85385675F9E3508630570C545DECCD1241C7A8FFF523D2AC500D6F68745E43975DBF871C99504100B8DD6715F036FA51EFF9EB8B79D1E31FD555E78FC
+Neo Legacy signature: 8A961AB366DFD9A3EB1ED5FD496E1CE818A67873162A7A21F663BA320324A8B075EAFA432926B7A894FB3DC5138BF0564A10D1AAC1A4B4E8DB8184CD69F829AC`
+
+var testMessagePhaSignatureIncorrect string = `I have signed this message with my Phantasma, Ethereum and Neo Legacy signatures to prove that following addresses belong to me and were derived from private key that belongs to me and to confirm my willingness to swap funds across these addresses upon my request. My public addresses are:
+Phantasma address: P2KHhbVZWDv1ZLLoJccN3PUAb9x9BqRnUyH3ZEhu5YwBeJQ
+Ethereum address: 0xDf738B927DA923fe0A5Fd3aD2192990C68913e6a
+Ethereum public key: 5D3F7F469803C68C12B8F731576C74A9B5308484FD3B425D87C35CAED0A2E398C7AC626D916A1D65E23F673A55E6B16FFC1ABD673F3EF6AE8D5E6A0F99784A56
+Neo Legacy address: Ae3aEA6CpvckvypAUShj2CLsy7sfynKUzj
+Neo Legacy public key: 183A301779007BF42DD7B5247587585B0524E13989F964C2A8E289A0CDC91F001765FCC3B4CEE5ED274C4A8B6D80978BDFED678210458CE264D4A4DAB3923EE6
+
+Phantasma signature: EEC5FFE0FE71C522E32A99C3C84F111944275D967BA74BA06749CB58EEC48A2E450B0ABCC59D86880AC01DA2AECFEF1FC5C45BFD840761FC420AF8194ACD5B01
+Ethereum signature: E3E1FCD85385675F9E3508630570C545DECCD1241C7A8FFF523D2AC500D6F68745E43975DBF871C99504100B8DD6715F036FA51EFF9EB8B79D1E31FD555E78FC
+Neo Legacy signature: 8A961AB366DFD9A3EB1ED5FD496E1CE818A67873162A7A21F663BA320324A8B075EAFA432926B7A894FB3DC5138BF0564A10D1AAC1A4B4E8DB8184CD69F829AC`
+
+func TestProofOfAddressesVerifier(t *testing.T) {
+	v := NewProofOfAddressesVerifier(testMessage)
+	success, errorMessage := v.VerifyMessage()
+	assert.Equal(t, true, success)
+	assert.Equal(t, "", errorMessage)
+
+	v = NewProofOfAddressesVerifier(testMessagePhaAddressIncorrect)
+	success, errorMessage = v.VerifyMessage()
+	assert.Equal(t, false, success)
+	assert.Equal(t, "Phantasma signature is incorrect!\nEthereum signature is incorrect!\n", errorMessage)
+
+	v = NewProofOfAddressesVerifier(testMessagePhaSignatureIncorrect)
+	success, errorMessage = v.VerifyMessage()
+	assert.Equal(t, false, success)
+	assert.Equal(t, "Phantasma signature is incorrect!\n", errorMessage)
+}
