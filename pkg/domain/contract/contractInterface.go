267e5c554b9008889bbaf7042f3dd2eb05584e83
alexmpa <alexander.paukonen@gmail.com>
2025-02-21 22:51:25 -0300

    More serialization options for some structs

--- pkg/domain/contract/contractInterface.go
+++ pkg/domain/contract/contractInterface.go
@@ -42,3 +42,41 @@ func (i *ContractInterface) Deserialize(reader *io.BinReader) {
 		i.Events[j] = e
 	}
 }
+
+type ContractInterface_S struct {
+	Methods *orderedmap.OrderedMap[string, ContractMethod_S]
+
+	Events []ContractEvent
+}
+
+func (i *ContractInterface_S) Serialize(writer *io.BinWriter) {
+	{
+		writer.WriteB(byte(i.Methods.Len()))
+		for pair := i.Methods.Oldest(); pair != nil; pair = pair.Next() {
+			pair.Value.Serialize(writer)
+		}
+
+		writer.WriteB(byte(len(i.Events)))
+		for _, evt := range i.Events {
+			evt.Serialize(writer)
+		}
+	}
+}
+
+func (i *ContractInterface_S) Deserialize(reader *io.BinReader) {
+	len := int(reader.ReadB())
+	i.Methods = orderedmap.New[string, ContractMethod_S]()
+	for j := 0; j < len; j++ {
+		method := ContractMethod_S{}
+		method.Deserialize(reader)
+		i.Methods.Set(method.Name, method)
+	}
+
+	len = int(reader.ReadB())
+	i.Events = make([]ContractEvent, len)
+	for j := 0; j < len; j++ {
+		e := ContractEvent{}
+		e.Deserialize(reader)
+		i.Events[j] = e
+	}
+}
--- pkg/domain/contract/contractMethod.go
+++ pkg/domain/contract/contractMethod.go
@@ -35,3 +35,39 @@ func (m *ContractMethod) Deserialize(reader *io.BinReader) {
 		m.Parameters[i] = ContractParameter{Name: pName, Type: pVMType}
 	}
 }
+
+type ContractMethod_S struct {
+	Name       string
+	ReturnType string
+	Parameters []ContractParameter
+	Offset     int32
+}
+
+func (m *ContractMethod_S) Serialize(writer *io.BinWriter) {
+	writer.WriteString(m.Name)
+
+	var vmType vm.VMType
+	writer.WriteB(byte(vmType.FromString(m.ReturnType)))
+
+	writer.WriteU32LE(uint32(m.Offset))
+	writer.WriteB(byte(len(m.Parameters)))
+	for _, entry := range m.Parameters {
+		writer.WriteString(entry.Name)
+		writer.WriteB(byte(entry.Type))
+	}
+}
+
+func (m *ContractMethod_S) Deserialize(reader *io.BinReader) {
+	m.Name = reader.ReadString()
+
+	m.ReturnType = vm.VMTypeLookup[vm.VMType(reader.ReadB())]
+
+	m.Offset = int32(reader.ReadU32LE())
+	len := int(reader.ReadB())
+	m.Parameters = make([]ContractParameter, len)
+	for i := 0; i < len; i++ {
+		var pName = reader.ReadString()
+		var pVMType = vm.VMType(reader.ReadB())
+		m.Parameters[i] = ContractParameter{Name: pName, Type: pVMType}
+	}
+}
